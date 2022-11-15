ffa462a61836c2fefb641c46eb8520113c95f58a
Hyejin <110078157+decaf-latte@users.noreply.github.com>
2022-11-15 17:12:50 +0900

    [level 0] Title: 자릿수 더하기, Time: 1.46 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -1,11 +1,6 @@
 import java.util.stream.Stream;
 class Solution {
     public int solution(int n) {
-int answer = 0;
-    int[] nums = Stream.of(String.valueOf(n).split("")).mapToInt(Integer::parseInt).toArray();
-    for (int i = 0; i < nums.length; i ++ ){
-      answer += nums[i];
-    }
-    return answer;
+return Stream.of(String.valueOf(n).split("")).mapToInt(Integer::parseInt).sum();
     }
 }
