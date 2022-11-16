9809128301a72f2f1052d4c54699ae179ed3131a
Hyejin <110078157+decaf-latte@users.noreply.github.com>
2022-11-16 21:47:44 +0900

    [level 0] Title: 대문자와 소문자, Time: 4.23 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.java
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.java
@@ -1,19 +1,19 @@
+import java.util.*;
+import java.util.stream.Collectors;
 class Solution {
     public String solution(String my_string) {
-       String answer = "";
-        char[] arry = my_string.toCharArray();
-		
-		String temp ="";
-		
-		for(int i = 0; i<arry.length; i++) {
-			if( arry[i] >= 97 && arry[i] <= 122 ) {
-				temp = arry[i] + "";
-				answer += temp.toUpperCase();
-			} else if( arry[i] >=65 && arry[i] <= 90){
-				temp = arry[i] + "";
-				answer += temp.toLowerCase();
-			}
-		}
-        return answer;
+       String[] a = my_string.split("");
+    String[] b = my_string.toUpperCase().split("");
+    String[] result = new String[a.length];
+
+    for(int i = 0 ; i < my_string.length(); i++){
+      if (Objects.equals(a[i], b[i])){
+        result[i] = a[i].toLowerCase();
+      }else {
+        result[i] = b[i];
+      }
     }
+    return Arrays.stream(result).collect(Collectors.joining());
+
+  }
 }
