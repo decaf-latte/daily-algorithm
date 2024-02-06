9bc829ec7760cb36fc09a62ce113717be4fb63c0
Hyejin <110078157+decaf-latte@users.noreply.github.com>
2024-02-07 08:56:20 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.02 ms, Memory: 70.2 MB -BaekjoonHub

--- 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.java
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -1,6 +1,20 @@
 class Solution {
     public int solution(int[] common) {
         int answer = 0;
+        int diff = 0;
+        
+        for (int i : common){
+            if (common[1]- common[0] == common [2] - common [1]){
+                diff = common[1]- common[0];
+                answer = common[common.length - 1] + diff;
+                
+            } else {
+                 diff = common[1] / common[0];
+                answer = common[common.length - 1] * diff;
+            }
+        }
+    
+        
         return answer;
     }
 }
